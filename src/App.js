de92ddfe8c2a370c731c99a52b7701cbbbc98125
sainathrx <sainathreddy984811@gmail.com>
2025-07-22 10:41:33 +0530

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function App() {
+  return (
+    <div>
+      <h1>Welcome to ElevateEdge 🚀</h1>
+      <p>Your Smart Skill Mapping Platform is Live!</p>
+    </div>
+  );
+}
